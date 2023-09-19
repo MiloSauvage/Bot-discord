3010daaeeb412339e2c1dd21101cb57388e5600c
MiloSauvage <desseilleloic@gmail.com>
2023-09-19 15:15:41 +0200

    readme file

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-token = "MTA2Mzc0MTU3MjQwMDgyMDMwNA.Gr62fS.ajiPEKc1cXbTu-fLJPTMBsJsXEtbi4ANJB2UTs"
+const { Client, Events, GatewayIntentBits } = require('discord.js');
+
+const dotenv = require("dotenv")
+dotenv.config();
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.once(Events.ClientReady, c => {
+	console.log(`Ready! Logged in as ${c.user.tag}`);
+});
+
+client.login(process.env.TOKEN);
